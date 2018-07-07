6ada762eaab47e9be339939fe418ecaa753cb5e5
polarsun <1806060082@qq.com>
2018-07-07 22:36:14 +0800

    solved problem 27

---
+++ src/main/java/cn/polarsun/RemoveElement.java
@@ -0,0 +1,14 @@
+package cn.polarsun;
+
+public class RemoveElement {
+	public int removeElement(int[] nums, int val) {
+		int i = 0;
+		for (int j = 0; j < nums.length; j++) {
+			if (val == nums[j]) {
+				continue;
+			}
+			nums[i++] = nums[j];
+		}
+		return i;
+	}
+}
