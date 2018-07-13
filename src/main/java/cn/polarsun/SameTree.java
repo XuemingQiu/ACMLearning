f869375f407f11b0e6c7923ec23201d4fd42d9ed
polarsun <1806060082@qq.com>
2018-07-13 09:57:04 +0800

    solved problem 100

---
+++ src/main/java/cn/polarsun/SameTree.java
@@ -0,0 +1,24 @@
+//package cn.polarsun;
+//
+//public class SameTree {
+//	/**
+//	 * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+//	 * left; TreeNode right; TreeNode(int x) { val = x; } }
+//	 */
+//	public boolean isSameTree(TreeNode p, TreeNode q) {
+//		if (p == null && q == null) {
+//			return true;
+//		}
+//		if (p == null && q != null) {
+//			return false;
+//		}
+//		if (p != null && q == null) {
+//			return false;
+//		}
+//		if (p.val != q.val) {
+//			System.out.println(p.val + "   " + q.val);
+//			return false;
+//		}
+//		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//	}
+//}
