fa8e501a8dfac7a5e1656b58af9b18c300cff9ad
polarsun <1806060082@qq.com>
2018-07-15 17:48:43 +0800

    solved problem 104

---
+++ src/main/java/cn/polarsun/MaximumDepthofBinaryTree.java
@@ -0,0 +1,10 @@
+package cn.polarsun;
+
+public class MaximumDepthofBinaryTree {
+//	public int maxDepth(TreeNode root) {
+//		if (root == null) {
+//			return 0;
+//		}
+//		return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+//	}
+}
