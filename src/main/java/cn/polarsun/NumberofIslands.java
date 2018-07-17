5e4c44f5cdd947e34d7d6e4419fb3cc4c605134c
polarsun <1806060082@qq.com>
2018-07-17 20:49:43 +0800

    solved problem 200

---
+++ src/main/java/cn/polarsun/NumberofIslands.java
@@ -0,0 +1,27 @@
+class NumberofIslands{
+	int[] xx = {0,-1,0,1};
+    int[] yy = {-1,0,1,0};
+    int ans = 0;
+    public int numIslands(char[][] grid) {
+        for(int i = 0;i < grid.length;i++){
+            for(int j = 0;j < grid[i].length;j++){
+                if(grid[i][j] == '1'){
+                    grid[i][j] = '0';
+                    dfs(i,j,grid);
+                    ans += 1;
+                }
+            }
+        }
+        return ans;
+    }
+    private void dfs(int x,int y,char[][] grid){
+        for(int i=0;i < 4;i++){
+            int x1 = x+xx[i];
+            int y1 = y+yy[i];
+            if(x1 >= 0 && x1 < grid.length && y1 >=0 && y1 < grid[0].length && grid[x1][y1] == '1'){
+                grid[x1][y1] = '0';
+                dfs(x1,y1,grid);
+            }
+        }
+    }
+}
